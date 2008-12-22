4d7d88cdcfc4c20e0f998dba90421f5b224eb8e0
typotemp <typotemp@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-12-22 17:24:24 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/as_freecap_read/trunk@15174 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "as_freecap_read"
+#
+# Auto generated 22-12-2008 16:43
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'freecap_read',
+	'description' => 'Improve readability of sr_freecap. Reduces morphing, less fade and smaller image',
+	'category' => 'fe',
+	'author' => 'Andreas Steinhuber',
+	'author_email' => 'andreas@typotemp.com',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'cms' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"9e9a";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"63a4";s:17:"ext_localconf.php";s:4:"63c4";s:28:"ext_typoscript_constants.txt";s:4:"fcff";s:24:"ext_typoscript_setup.txt";s:4:"f420";s:19:"doc/wizard_form.dat";s:4:"c40f";s:20:"doc/wizard_form.html";s:4:"751b";s:33:"pi1/class.ux_tx_srfreecap_pi1.php";s:4:"aecc";}',
+	'suggests' => array(
+	),
+);
+
+?>
